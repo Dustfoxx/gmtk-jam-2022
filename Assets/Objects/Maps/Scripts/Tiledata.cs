524874016036084ede006b435ac3efa219d5df5e
Dustfoxx <fox.hamren@gmail.com>
2022-07-16 14:21:39 +0200

    Added tilemap functions

--- Assets/Objects/Maps/Scripts/Tiledata.cs
+++ Assets/Objects/Maps/Scripts/Tiledata.cs
@@ -10,8 +10,15 @@ public class Tiledata : ScriptableObject
     
     public TileBase[] tiles;
 
-    public bool walkable, rollable;
+    public bool walkable, rollable, wall;
 
+    public GameObject wallObject = null;
 
+    // void Start(){
+    //     if(wall){
+    //         GameObject newBox = Instantiate(wallObject);
+    //         newBox.transform.position = new Vector3(tiles[0].position.x, tiles[0].position.y, tiles[0].position.z);
+    //     }
+    // }
 
 }
